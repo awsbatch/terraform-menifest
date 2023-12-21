cb88165132a64310807e5ded7155e0aff543c84a
root <root@ip-172-31-36-78.ap-south-1.compute.internal>
2023-12-21 07:46:17 +0000

    varibales-inputs 2st examples

--- input_vars_basic/variables.tf
+++ input_vars_basic/variables.tf
@@ -1,4 +1,25 @@
-variable "aws-ami" {}
-variable "intance-type" {}
-variable "az" {}
+#variable "aws-ami" {}
+#variable "intance-type" {}
+#variable "az" {}
 
+variable "aws-ami" {
+    description = "This is for ec2 instance image"
+    type = string
+    default = "ami-023c11a32b0207432"
+}
+variable "intance-type" {
+    description = "this field use for instance type"
+    type = string
+    default = "t2.micro"
+}
+variable "az" {
+    description = "this field for region in which region you want spin you servers"
+    type = string
+    default = "us-east-1b"
+}
+
+/*
+export TF_VARS_instance-type=t2.micro
+export TF_VARS_az=us-east-1a
+
+*/
